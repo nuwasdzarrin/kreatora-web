a2ac2a9e118af252c8bd35abbbe0b40453dab77a
nuwasdzarrin <nuwasdzarrin3@gmail.com>
2021-10-02 05:30:17 +0700

    add laravel command

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,42 @@
 |
 */
 
+Route::get('run_cache', function () {
+    try {
+        \Illuminate\Support\Facades\Artisan::call('cache:clear');
+        dd("Success to cache");
+    } catch (Exception $exception){
+        throw $exception;
+    }
+});
+
+Route::get('run_config', function () {
+    try {
+        \Illuminate\Support\Facades\Artisan::call('config:clear');
+        dd("Success to config");
+    } catch (Exception $exception){
+        throw $exception;
+    }
+});
+
+Route::get('run_migrate', function () {
+    try {
+        \Illuminate\Support\Facades\Artisan::call('migrate');
+        dd("Success to migrate");
+    } catch (Exception $exception){
+        throw $exception;
+    }
+});
+
+Route::get('run_storage', function () {
+    try {
+        \Illuminate\Support\Facades\Artisan::call('storage:link');
+        dd("Success to storage link");
+    } catch (Exception $exception){
+        throw $exception;
+    }
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
