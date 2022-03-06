40e279c3c1167a47945a19f41ec9d530e4e5cc79
nuwasdzarrin <nuwasdzarrin3@gmail.com>
2022-03-06 13:50:48 +0700

    update

--- resources/assets/js/pages/campaigns/CampaignDetail.vue
+++ resources/assets/js/pages/campaigns/CampaignDetail.vue
@@ -30,22 +30,33 @@
         <div v-html="dataSection" v-show="isSection === 'description' || isSection === 'risk'"/>
         <div class="pt-3" v-show="isSection === 'faq'">
           <div class="mb-4" v-for="(item, index) in detail_campaign.faqs" :key="index">
-            <div class="faq-question" :class="{'no-bottom-radius': is_faq_open[index]}">
+            <div class="faq-question" :class="{'no-bottom-radius': is_faq_open[index]}" @click="openFaq(index)">
               <div style="max-width: 90%">
                 {{ item.question }}
               </div>
-              <div @click="openFaq(index)"><i class="fas fa-chevron-right" :class="{'fa-rotate-90': is_faq_open[index]}"></i></div>
+              <div><i class="fas fa-chevron-right" :class="{'fa-rotate-90': is_faq_open[index]}"></i></div>
             </div>
             <div class="faq-answer" v-show="is_faq_open[index]">
               {{ item.answer }}
             </div>
           </div>
         </div>
+        <div class="pt-3" v-show="isSection === 'update'">
+          <div class="update-box" v-for="(item, index) in detail_campaign.updates" :key="index">
+            <div class="d-flex justify-content-between mb-3 text-12">
+              <div><strong>Update #{{item.index}}</strong></div>
+              <div style="color: #5C5C70">{{item.updated_at | dateDDMMYYYY}}</div>
+            </div>
+            <h5><strong>{{item.title}}</strong></h5>
+            <p class="text-14" v-html="item.description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisi massa tortor vel justo vivamus aliquet. Sed massa lectus sed neque, maecenas enim porttitor ......</p>
+            <div class="text-right"><span class="mr-2" style="color: black">11</span><i class="far fa-comment-alt mr-4"></i><span class="mr-2" style="color: black">11</span><i class="far fa-heart"></i></div>
+          </div>
+        </div>
       </div>
     </div>
     <div v-else>
       <div style="position: relative;">
-        <img :src="`/storage/${detail_campaign.images[0]}`" alt="campaign-images" style="width: 100%; height: 215px;">
+        <img :src="detail_campaign.images ? `/storage/${detail_campaign.images[0]}` : '/assets_homepage/img/hero-img.png'" alt="campaign-images" style="width: 100%; height: 215px;">
         <span class="back-button-img" @click="$router.push({ name: 'HomePage'})">
           <i class="fas fa-arrow-left"></i>
         </span>
@@ -219,5 +230,10 @@ export default {
   background: #D6E1E8;
   border-bottom-left-radius: 12px;
   border-bottom-right-radius: 12px;
+}
+.update-box {
+  padding: 20px 10px;
+  box-shadow: 4px 4px 15px rgba(0, 0, 0, 0.12);
+  border-radius: 12px;
 }
 </style>
--- routes/web.php
+++ routes/web.php
@@ -61,6 +61,6 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('shipping', 'ShippingController')->name('shipping');
 
-Route::get('/campaign/', function () {
+Route::get('/app', function () {
     return view('app');
 });
