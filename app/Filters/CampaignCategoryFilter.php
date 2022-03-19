15cdbbecb80477fce1165dd2476701d043c7745d
nuwasdzarrin <nuwasdzarrin3@gmail.com>
2022-03-19 21:35:07 +0700

    add campaign category filter

--- app/Filters/CampaignCategoryFilter.php
+++ app/Filters/CampaignCategoryFilter.php
@@ -23,6 +23,7 @@ class CampaignCategoryFilter extends BaseFilter
      */
     protected $sortables = [
         'id',
+        'sorting',
         'name',
         'created_at',
         'updated_at'
