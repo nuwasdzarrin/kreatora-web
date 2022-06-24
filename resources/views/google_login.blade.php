f2227482885cea517cad692a0e5dc1917b9a09d8
nuwasdzarrin <nuwasdzarrin3@gmail.com>
2022-06-24 22:59:16 +0700

    update test google login

--- resources/views/google_login.blade.php
+++ resources/views/google_login.blade.php
@@ -8,7 +8,17 @@
 </head>
 <body>
 <script src="https://accounts.google.com/gsi/client" async defer></script>
+<script src="https://apis.google.com/js/platform.js" async defer></script>
 <script>
+    function onSignIn(googleUser) {
+        var id_token = googleUser.getAuthResponse().id_token;
+        var profile = googleUser.getBasicProfile();
+        console.log("id token: ", id_token);
+        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    }
     function handleCredentialResponse(response) {
         console.log("Encoded JWT ID token: ", response ,response.credential);
     }
@@ -25,5 +35,6 @@
     }
 </script>
 <div id="buttonDiv"></div>
+<div class="g-signin2" data-onsuccess="onSignIn"></div>
 </body>
 </html>
