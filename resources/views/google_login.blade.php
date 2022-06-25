0aba1270c1a73cbbbb64e9a9893dad198d433b82
nuwasdzarrin <nuwasdzarrin3@gmail.com>
2022-06-25 10:26:16 +0700

    clean up google login

--- resources/views/google_login.blade.php
+++ resources/views/google_login.blade.php
@@ -4,22 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="google-signin-client_id" content="879549587031-ohodmoqbqe3lu01jng08j6v3dj09rb09.apps.googleusercontent.com">
     <title>Google Login Asuuu</title>
 </head>
 <body>
 <script src="https://accounts.google.com/gsi/client" async defer></script>
-<script src="https://apis.google.com/js/platform.js" async defer></script>
 <script>
-    function onSignIn(googleUser) {
-        var id_token = googleUser.getAuthResponse().id_token;
-        var profile = googleUser.getBasicProfile();
-        console.log("id token: ", id_token);
-        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-        console.log('Name: ' + profile.getName());
-        console.log('Image URL: ' + profile.getImageUrl());
-        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    }
     function handleCredentialResponse(response) {
         console.log("Encoded JWT ID token: ", response ,response.credential);
     }
@@ -30,12 +19,11 @@
         });
         google.accounts.id.renderButton(
             document.getElementById("buttonDiv"),
-            { theme: "outline", size: "large" }  // customization attributes
+            { theme: "outline", size: "large", type: "icon" }  // customization attributes
         );
         google.accounts.id.prompt(); // also display the One Tap dialog
     }
 </script>
 <div id="buttonDiv"></div>
-<div class="g-signin2" data-onsuccess="onSignIn"></div>
 </body>
 </html>
