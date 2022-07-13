317ca228e7be14fa5eb8ab28a0890dd356077b36
nuwasdzarrin <nuwasdzarrin3@gmail.com>
2022-07-13 21:25:57 +0700

    update client google 2

--- config/services.php
+++ config/services.php
@@ -36,8 +36,8 @@ return [
     ],
 
     'google' => [
-        'client_id' => env('GOOGLE_CLIENT_ID','362844505116-f9pil6mf5abiuoqahv815h3ul1gvj9ru.apps.googleusercontent.com'),
-        'client_secret' => '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',
+        'client_id' => env('GOOGLE_CLIENT_ID','362844505116-9hsro776fj7h7pugpnkvmjao35ndf3ha.apps.googleusercontent.com'),
+        'client_secret' => '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',
 //        'client_id' => env('GOOGLE_CLIENT_ID','879549587031-ohodmoqbqe3lu01jng08j6v3dj09rb09.apps.googleusercontent.com'),
 //        'client_secret' => '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',
         'redirect' => 'http://127.0.0.1:8000/google/callback',
--- resources/assets/js/config/index.js
+++ resources/assets/js/config/index.js
@@ -4,7 +4,7 @@ export default {
 		'Authorization' : 'Bearer '+ localStorage.getItem("token"),
 	},
 	google: {
-		client_id: "362844505116-f9pil6mf5abiuoqahv815h3ul1gvj9ru.apps.googleusercontent.com"
+		client_id: "362844505116-9hsro776fj7h7pugpnkvmjao35ndf3ha.apps.googleusercontent.com"
 		// client_id: "879549587031-ohodmoqbqe3lu01jng08j6v3dj09rb09.apps.googleusercontent.com"
 	},
 	isProduction: false
