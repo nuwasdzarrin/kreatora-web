4cb7ef6fabd6617273d558e2fffd08e008399e64
nuwasdzarrin <nuwasdzarrin3@gmail.com>
2022-07-16 10:13:13 +0700

    update avatar campaign and support

--- resources/assets/js/config/index.js
+++ resources/assets/js/config/index.js
@@ -7,5 +7,5 @@ export default {
 		client_id: "362844505116-9hsro776fj7h7pugpnkvmjao35ndf3ha.apps.googleusercontent.com"
 		// client_id: "879549587031-ohodmoqbqe3lu01jng08j6v3dj09rb09.apps.googleusercontent.com"
 	},
-	isProduction: false
+	isProduction: true
 }
